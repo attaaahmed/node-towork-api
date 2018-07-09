f9c574fea682d5fd5728a09ba5b71dbac074b21b
Ahmed A. Alshammary <attaaahmed@gmail.com>
2018-07-10 00:53:12 +0300

    inital state

---
+++ playground/mongo.js
@@ -0,0 +1,42 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const { MongoClient, ObjectID } = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    const database = db.db('Users') // i was missing this Argument
+    console.log('Connected to MongoDB server');
+
+    // data.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     } else {
+    //         console.log(JSON.stringify(result.ops, undefined, 2));
+    //     }
+    // })
+
+    // Insert new doc into Users (name, age, location)
+
+    // database.collection('Users').insertOne({
+    //     Name: 'Ahmed A. Alshammary',
+    //     Age: 23,
+    //     Likeprogramming: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to Creat the database');
+    //     }
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // })
+
+    db.close();
+    console.log('Successfuly exiting the database');
+});
