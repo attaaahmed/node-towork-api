a573d55ddb4a94502351a39ab0e65537a850671a
Ahmed A. Alshammary <attaaahmed@gmail.com>
2018-08-03 15:25:06 +0300

    add the query file in the playground

---
+++ playground/mongoqueries.js
@@ -0,0 +1,33 @@
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/Todo');
+const { ObjectID } = require('mongodb');
+var id = '5b5daefaa5d39c1b2744bf2311'; // this id is fore quering 
+
+if (!ObjectID.isValid(id)) {
+    console.log('Id is not valid')
+}
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     if (!todos) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todos are ', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo is ', todo);
+// });
+
+Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return console.log('Id not found');
+    }
+    console.log(todo);
+}).catch((e) => { console.log('error message is', e); });
